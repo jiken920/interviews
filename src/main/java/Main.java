8152f7becf9530d17ec1e3dd174c8e2895183e29
krudio <krudio@gmail.com>
2017-03-20 16:57:18 -0700

    Logger rate limiter

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,4 @@
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * Created by Kenji on 10/19/2016.
@@ -293,5 +291,26 @@ public class Main {
         }
 
         return sb.toString();
+    }
+
+    public class Logger {
+        private Map<String, Integer> messages;
+
+        /** Initialize your data structure here. */
+        public Logger() {
+            this.messages = new HashMap<>();
+        }
+
+        /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+         If this method returns false, the message will not be printed.
+         The timestamp is in seconds granularity. */
+        public boolean shouldPrintMessage(int timestamp, String message) {
+            if(messages.containsKey(message) && timestamp - messages.get(message) < 10) {
+                return false;
+            } else {
+                messages.put(message, timestamp);
+                return true;
+            }
+        }
     }
 }
