038a61e20072d52534c269f531211750513401c4
krudio <krudio@gmail.com>
2017-03-27 15:43:25 -0700

    Majority Element

---
+++ src/main/java/MajorityElement.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by krudio on 3/27/17.
+ */
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+        int majority = 0;
+
+        Map<Integer, Integer> counts = new HashMap<>();
+        for(int num : nums) {
+            if(counts.containsKey(num)) {
+                int count = counts.get(num);
+                counts.put(num, count + 1);
+            } else {
+                counts.put(num, 1);
+            }
+
+            if(counts.get(num) > nums.length / 2) {
+                majority = num;
+                break;
+            }
+        }
+
+        return majority;
+    }
+}
