c89b6473404feb0a348077c3173d00b708b17f63
krudio <krudio@gmail.com>
2017-04-16 17:20:21 -0700

    Count Battleships

---
+++ src/main/java/CountBattleships.java
@@ -0,0 +1,28 @@
+/**
+ * Created by krudio on 4/16/17.
+ */
+public class CountBattleships {
+//    Given an 2D board, count how many battleships are in it. The battleships are represented with 'X's, empty slots
+//    are represented with '.'s. You may assume the following rules:
+//
+//    * You receive a valid board, made of only battleships or empty slots.
+//    * Battleships can only be placed horizontally or vertically. In other words, they can only be made of the shape
+//          1xN (1 row, N columns) or Nx1 (N rows, 1 column), where N can be of any size.
+//    * At least one horizontal or vertical cell separates between two battleships - there are no adjacent battleships.
+    public int countBattleships(char[][] board) {
+        if(board == null || board.length == 0) return 0;
+
+        int count = 0;
+
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[0].length; col++) {
+                if(board[row][col] == '.') continue;
+                if(row > 0 && board[row - 1][col] == 'X') continue;
+                if(col > 0 && board[row][col - 1] == 'X') continue;
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
