d4ce9347f293cba8f152bee046b6547b5c078524
krudio <krudio@gmail.com>
2017-04-20 11:05:52 -0700

    Palindrome Permutation

---
+++ src/main/java/PalindromePermutation.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by krudio on 4/20/17.
+ *
+ * Given a string, determine if a permutation of the string could form a palindrome.
+
+     For example,
+     "code" -> False, "aab" -> True, "carerac" -> True.
+ */
+public class PalindromePermutation {
+    public boolean canPermutePalindrome(String s) {
+        if(s == null || s.isEmpty()) return false;
+
+        Set<Character> vals = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(vals.contains(c)) {
+                vals.remove(c);
+            } else {
+                vals.add(c);
+            }
+        }
+
+        return vals.size() == 0 || vals.size() == 1;
+    }
+}
