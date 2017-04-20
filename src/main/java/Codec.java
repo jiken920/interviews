61117c90bfcfe1271e7ddc004547b3c17e3d6b00
krudio <krudio@gmail.com>
2017-04-20 10:30:27 -0700

    Encode/deocde URL shortener

---
+++ src/main/java/Codec.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by krudio on 4/20/17.
+ */
+public class Codec {
+    private static final String baseUrl = "http://tinyurl.com/";
+    private final Map<String, String> hashCodes = new HashMap<>();
+
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        int code = longUrl.hashCode();
+        String shortUrl = baseUrl + String.valueOf(code);
+        hashCodes.put(shortUrl, longUrl);
+        return shortUrl;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return hashCodes.get(shortUrl);
+    }
+}
