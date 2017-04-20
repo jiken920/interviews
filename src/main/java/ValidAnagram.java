60dc3744223c0bf6da42350a633eef10d497b160
krudio <krudio@gmail.com>
2017-04-20 10:42:33 -0700

    Valid Anagram

---
+++ src/main/java/ValidAnagram.java
@@ -0,0 +1,28 @@
+/**
+ * Created by krudio on 4/20/17.
+ */
+public class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+
+        s = s.toLowerCase();
+        t = t.toLowerCase();
+
+        int[] counts = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            int charIdx = s.charAt(i) - 'a';
+            counts[charIdx]++;
+        }
+
+        for (int j = 0; j < t.length(); j++) {
+            int charIdx = t.charAt(j) - 'a';
+            counts[charIdx]--;
+        }
+
+        for(int count : counts) {
+            if(count != 0) return false;
+        }
+
+        return true;
+    }
+}
