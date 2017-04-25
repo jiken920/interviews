390f7b00ce80363c00302c0330e8371eff94b193
krudio <krudio@gmail.com>
2017-04-25 16:28:46 -0700

    Sorted Array to BST

---
+++ src/main/java/SortedArrayToBST.java
@@ -0,0 +1,20 @@
+/**
+ * Created by krudio on 4/25/17.
+ */
+public class SortedArrayToBST {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if(nums == null || nums.length == 0) return null;
+        return sortedArrayToBST(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode sortedArrayToBST(int[] nums, int min, int max) {
+        if(min > max) return null;
+
+        int mid = (min + max) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = sortedArrayToBST(nums, min, mid - 1);
+        node.right = sortedArrayToBST(nums, mid + 1, max);
+
+        return node;
+    }
+}
---
+++ src/test/java/SortedArrayToBSTTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by krudio on 4/25/17.
+ */
+public class SortedArrayToBSTTest {
+    @Test
+    public void sortedArrayToBST() throws Exception {
+        SortedArrayToBST main = new SortedArrayToBST();
+        TreeNode root = main.sortedArrayToBST(new int[]{1, 3});
+
+    }
+
+}
