31fefe7f6e9445b76e6edb30ab8b15131af506e1
krudio <krudio@gmail.com>
2017-05-18 15:18:52 -0700

    Move Game Of Life to separate project

--- src/main/java/Pattern.java
+++
@@ -1,8 +0,0 @@
-import com.sun.javaws.exceptions.InvalidArgumentException;
-
-/**
- * Created by krudio on 5/15/17.
- */
-public interface Pattern {
-    void init(boolean[][] board) throws InvalidArgumentException;
-}
